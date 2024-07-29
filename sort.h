2bc71222c4f1474f450df5cfa8bd4ed1fad646ad
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-07-29 08:54:56 +0300

    Fix bug in code

--- sort.h
+++ sort.h
@@ -24,7 +24,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-int lomuto_partition(int *array, int low, int high);
 void quick_sort_recursive(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
